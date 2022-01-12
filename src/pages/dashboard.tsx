277c8367ca9509e4d7259a424196059ae24f8604
Lelberto <jsurieux@gmail.com>
2022-01-12 16:02:45 +0100

    Router implementation Add home and dashboard pages Add routing condition if the user is authenticated or not

---
+++ src/pages/dashboard.tsx
@@ -0,0 +1,8 @@
+import { FC } from 'react';
+
+/**
+ * Dashboard page.
+ */
+export const DashboardPage: FC = () => (
+  <div>Dashboard</div>
+);
---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+import { FC } from 'react';
+import { AuthenticationContainer } from '../components/authentication-container';
+
+/**
+ * Home page.
+ */
+export const HomePage: FC = () => (
+  <AuthenticationContainer />
+);
